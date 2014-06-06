7e605472539b5c8f8026b567805d577be0404052
Song <nosongyang@gmail.com>
2014-06-06 12:52:11 +0800

    change the port

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var errorLogfile = fs.createWriteStream('error.log', {flags:'a'});
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5050);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
